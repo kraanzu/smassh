c99f7979cb1916103296aa5de89cbc7c377897db
kraanzu <kraanzu@gmail.com>
2022-02-07 13:15:05 +0530

    Some Improvements + feat: Add cursor buddy

--- ui/widgets/screen.py
+++ ui/widgets/screen.py
@@ -1,11 +1,13 @@
 from typing import Literal
 from bisect import bisect
+from os import get_terminal_size
 
 from rich.text import Span, Text
 from rich.panel import Panel
 from textual.app import App
 from textual.widget import Widget
 from textual.message import Message, MessageTarget
+from utils import chomsky, Parser
 
 
 class FinishedTyping(Message, bubble=True):
@@ -13,14 +15,16 @@ class FinishedTyping(Message, bubble=True):
         super().__init__(sender)
 
 
+empty_span = Span(0, 0, "")
+
+
 class Screen(Widget):
     def __init__(
         self,
-        paragraph: str,
         speed_threshold: int = 0,
         accuracy_threshhold: int = 0,
         min_burst: int = 0,
-        cursor_buddy_speed: int | None = None,
+        cursor_buddy_speed: int = 190,
         force_correct: bool = False,
         tab_reset: bool = False,
         difficulty: Literal["normal", "expert", "master"] = "normal",
@@ -31,9 +35,8 @@ class Screen(Widget):
         caret_style: Literal["underline", "block", "off"] = "off",
     ):
         super().__init__()
-        self.paragraph = Text(paragraph, spans=[Span(0, 0, "magenta")])
-        self.paragraph.append(" " + str(self._size))
 
+        self.set_paragraph()
         self.speed_threshold = speed_threshold
         self.accuracy_threshhold = accuracy_threshhold
         self.min_burst = min_burst
@@ -51,9 +54,43 @@ class Screen(Widget):
             len(self.paragraph.plain)
         ]
         self.cursor_position = 0
+        self.cursor_buddy_position = 0
         self.total_key_presses = 0
         self.mistakes = 0
         self.mistakes_hashmap = dict()
+
+        if self.cursor_buddy_speed:
+            self.set_interval(
+                60 / (5 * self.cursor_buddy_speed), self.move_cursor_buddy
+            )
+
+    def move_cursor_buddy(self):
+        if self.cursor_buddy_position < len(self.paragraph.plain) - 1:
+            self.cursor_buddy_position += 1
+            self.refresh()
+
+    def reset_screen(self):
+        self.cursor_position = 0
+        if self.repeat_same:
+            self.paragraph.spans = self.paragraph.spans[:1]
+        else:
+            self.set_paragraph()
+        self.refresh()
+
+    def set_paragraph(self):
+        self.paragraph_size = Parser().get_data("paragraph_size")
+        if self.paragraph_size == "teensy":
+            times = 2
+        elif self.paragraph_size == "small":
+            times = 5
+        elif self.paragraph_size == "big":
+            times = 10
+        else:
+            times = 15
+
+        paragraph = chomsky(times, get_terminal_size()[0] - 5)
+        self.paragraph = Text(paragraph)
+        self.refresh()
 
     def key_add(self, key: str):
 
@@ -69,9 +106,16 @@ class Screen(Widget):
         elif len(key) == 1:
             if key == " ":
                 if self.paragraph.plain[self.cursor_position] != " ":
-                    self.cursor_position = self.spaces[
-                        bisect(self.spaces, self.cursor_position)
-                    ]
+                    next_space = self.spaces[bisect(self.spaces, self.cursor_position)]
+                    self.paragraph.spans.extend(
+                        [empty_span]
+                        * (
+                            next_space - self.cursor_position + 1
+                        )  # 1 for the next space
+                    )
+                    self.cursor_position = next_space
+                else:
+                    self.paragraph.spans.append(empty_span)
 
             elif key == self.paragraph.plain[self.cursor_position]:
                 self.paragraph.spans.append(
@@ -92,7 +136,16 @@ class Screen(Widget):
             Text(
                 self.paragraph.plain,
                 spans=self.paragraph.spans
-                + [Span(self.cursor_position, self.cursor_position + 1, "reverse")],
+                + [Span(self.cursor_position, self.cursor_position + 1, "reverse")]
+                + [
+                    Span(
+                        self.cursor_buddy_position,
+                        self.cursor_buddy_position + 1,
+                        "reverse magenta",
+                    )
+                    if self.cursor_buddy_speed
+                    else Span(0, 0, "")
+                ],
             )
         )
 
@@ -101,7 +154,7 @@ if __name__ == "__main__":
 
     class MyApp(App):
         async def on_mount(self):
-            self.x = Screen("hi")
+            self.x = Screen()
             await self.view.dock(self.x)
 
     MyApp.run()
