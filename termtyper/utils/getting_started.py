685cae5eca08854f1175eb2c1b31ed9f19abe0ea
kraanzu <kraanzu@gmail.com>
2022-07-09 11:26:05 +0530

    chore: Update `Getting Started`

--- termtyper/utils/getting_started.py
+++ termtyper/utils/getting_started.py
@@ -9,7 +9,6 @@ ascii = """
 └─┘└─┘ ┴  ┴ ┴┘└┘└─┘  └─┘ ┴ ┴ ┴┴└─ ┴ └─┘─┴┘
 """
 
-
 GETTING_STARTERD_BANNER = Static(
     Panel(
         Align.center(ascii),
@@ -17,6 +16,10 @@ GETTING_STARTERD_BANNER = Static(
         border_style="bold magenta",
     )
 )
+
+
+def keybind(key: str, desc: str) -> str:
+    return f"{colored('●', 'green')} {colored(key,'bold blue')} => {colored(desc,'b' )}"
 
 
 def colored(text: str, color: str) -> str:
@@ -51,31 +54,22 @@ press {colored("ctrl+h", "bold blue")} once in the setting menu
 
 Some basic keybindings while in typing space:
 
- - pressing {colored("ctrl+z", "bold blue")} will toggle race-bar details
+ {keybind("ctrl+z","toggle race-bar details" )}
 
- - pressing {colored("ctrl+b", "bold blue")} will open up a bar theme menu
+ {keybind("ctrl+b","open up a bar theme menu" )}
 
- - pressing {colored("ctrl+s", "bold blue")} will open ap a size/time change menu
+ {keybind("ctrl+s","open up a size/time change menu" )}
 
- - pressing {colored("ctrl+o", "bold blue")} will open up a mode change menu
+ {keybind("ctrl+o","open up a mode change menu" )}
 
- - pressing {colored("ctrl+w", "bold blue")} will delete a whole word
+ {keybind("ctrl+w","delete a whole word" )}
+
  eg:
 
      {colored("This is h", "green")}{colored("ello", "red")}{colored("W", "reverse")}orld
 
      after pressing {colored("ctrl+w", "bold blue")}:
      {colored("This is", "green")} {colored("h", "reverse")}elloWorld
-
-{seperator}
-
- - pressing {colored("ctrl+l", "bold blue")} will delete a whole line
- eg:
-
-     {colored("This is first line. This is h", "green")}{colored("ello", "red")}{colored("W", "reverse")}orld
-
-     after pressing {colored("ctrl+l", "bold blue")}:
-     {colored("This is first line.", "green")} {colored("T", "reverse")}his is helloWorld
 
 Also, you can exit the application by pressing {colored("ctrl+q", "bold blue")} :)
 
