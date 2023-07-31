2b603b0319ae9a0aeffc386a0181e3d31809064b
kraanzu <kraanzu@gmail.com>
2023-07-31 21:49:52 +0530

    remove old widgets

--- termtyper/ui/screens/__init__.py
+++ termtyper/ui/screens/__init__.py
@@ -1,6 +1,6 @@
 from .about import AboutScreen
 from .settings import SettingsScreen
-from .help_menu import HelpScreen
+from .help import HelpScreen
 from .typing import TypingScreen
 
 __all__ = ["AboutScreen", "SettingsScreen", "HelpScreen", "TypingScreen"]
--- termtyper/ui/screens/settings.py
+++ termtyper/ui/screens/settings.py
@@ -1,6 +1,4 @@
-from textual.app import ComposeResult, events
 from textual.screen import Screen
-from termtyper.ui.settings_options import menu
 
 
 class SettingsScreen(Screen):
