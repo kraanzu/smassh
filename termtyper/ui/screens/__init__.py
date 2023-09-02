2c6d62821f2de18e384f2c5657316d0fa640649a
kraanzu <kraanzu@gmail.com>
2023-09-02 23:05:55 +0530

    refactor: remove multiple screens in favour of `ContentSwitcher`

--- termtyper/ui/screens/__init__.py
+++
@@ -1,6 +0,0 @@
-from .about import AboutScreen
-from .settings import SettingsScreen
-from .help import HelpScreen
-from .typing import TypingScreen
-
-__all__ = ["AboutScreen", "SettingsScreen", "HelpScreen", "TypingScreen"]
