ee702b4f5d06d56fa4ca80714eaf88bd6f3135bb
kraanzu <kraanzu@gmail.com>
2023-11-13 00:07:59 +0530

    hide overflow

--- termtyper/ui/screens/typing.py
+++ termtyper/ui/screens/typing.py
@@ -1,7 +1,15 @@
 from textual.app import ComposeResult
 from termtyper.ui.widgets.base_scroll import BaseScroll
+from termtyper.ui.widgets.config_strip import TypingConfigStrip
 
 
 class TypingScreen(BaseScroll):
+
+    DEFAULT_CSS = """
+    TypingScreen {
+        overflow: hidden;
+    }
+    """
+
     def compose(self) -> ComposeResult:
-        return super().compose()
+        yield TypingConfigStrip()
