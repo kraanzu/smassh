31d58f7b37fe88458ea716c359caae5cc50c459d
kraanzu <kraanzu@gmail.com>
2023-11-13 23:08:16 +0530

    feat: add generator class beta

--- termtyper/src/generator.py
+++ termtyper/src/generator.py
@@ -1,58 +1,75 @@
-from .words import english_words, french_words
-from random import choice, random, randint
+import textwrap
+from typing import List
+from termtyper.assets.words import *  # noqa
+from random import random, randint, sample
 
 
 PUNCS = "$%&'(),-.:;?"
 
 
-def generate(
-    times: int = 1,
-    numbers: bool = False,
-    punctuations: bool = False,
-    capitalizitions: str = "off",
-    language: str = "english",
-) -> str:
-    """
-    produces a paragraph
-    """
-    times *= 30
-    extra = []
-    if numbers:
-        extra.extend([str(choice(range(1, 10000))) for _ in " " * 200])
-    if punctuations:
-        extra.extend([choice(PUNCS) for _ in " " * 200])
+class Generator:
+    def __init__(self) -> None:
+        self.settings = {}
 
-    match language:
-        case "french":
-            words = french_words
-        case _:
-            words = english_words
+    @property
+    def words(self) -> str:
+        return " ".join(sample(english_words.split(), 64))
 
-    words = words.split()
-    arr = [choice(words + extra) for _ in " " * times]
-    para = ""
-    for i in arr:
-        if i in PUNCS:
-            para += i
-        else:
-            match capitalizitions:
-                case "on":
-                    if random() > 0.7:
-                        i = i.capitalize()
-                case "max":
-                    r = random()
-                    if r > 0.7:
-                        i = i.capitalize()
-                    elif r > 0.6:
-                        i = i.upper()
-                    elif r > 0.4:
-                        upper_indexs = [
-                            randint(0, len(i) - 1) for _ in range(randint(1, len(i)))
-                        ]
-                        for index in upper_indexs:
-                            i = i[:index] + i[index].upper() + i[index + 1 :]
-                case _:
-                    pass
-            para += " " + i
+    def generate(self) -> str:
+        lines = textwrap.wrap(self.words, 100)
+        return "\n".join(lines)
 
-    return para.strip()
+
+master_generator = Generator()
+
+# def generate_paragraph(
+#     times: int = 1,
+#     numbers: bool = False,
+#     punctuations: bool = False,
+#     capitalizitions: str = "off",
+#     language: str = "english",
+# ) -> str:
+#     """
+#     produces a paragraph
+#     """
+#     times *= 30
+#     extra = []
+#     if numbers:
+#         extra.extend([str(choice(range(1, 10000))) for _ in " " * 200])
+#     if punctuations:
+#         extra.extend([choice(PUNCS) for _ in " " * 200])
+#
+#     match language:
+#         case "french":
+#             words = french_words
+#         case _:
+#             words = english_words
+#
+#     words = words.split()
+#     arr = [choice(words + extra) for _ in " " * times]
+#     para = ""
+#     for i in arr:
+#         if i in PUNCS:
+#             para += i
+#         else:
+#             match capitalizitions:
+#                 case "on":
+#                     if random() > 0.7:
+#                         i = i.capitalize()
+#                 case "max":
+#                     r = random()
+#                     if r > 0.7:
+#                         i = i.capitalize()
+#                     elif r > 0.6:
+#                         i = i.upper()
+#                     elif r > 0.4:
+#                         upper_indexs = [
+#                             randint(0, len(i) - 1) for _ in range(randint(1, len(i)))
+#                         ]
+#                         for index in upper_indexs:
+#                             i = i[:index] + i[index].upper() + i[index + 1 :]
+#                 case _:
+#                     pass
+#             para += " " + i
+#
+#     return para.strip()
