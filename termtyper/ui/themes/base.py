d907b46caba41a1da02b5752ca5cdc8bade21bb0
kraanzu <kraanzu@gmail.com>
2023-11-24 11:20:55 +0530

    feat: Add themes

---
+++ termtyper/ui/themes/base.py
@@ -0,0 +1,24 @@
+class Theme:
+    # BASIC COLORS
+    WHITE: str
+    BLACK: str
+    RED: str
+    GREEN: str
+    YELLOW: str
+    BLUE: str
+    MAGENTA: str
+    CYAN: str
+
+    # SPECFCIC COLORS
+    SCREEN_BG: str
+    HEADER_BG: str
+    BRAND: str
+    NAVITEM_HOVER: str
+    NAVITEM_ACTIVE: str
+    HELP_HEADER: str
+    HELP_TEXT: str
+    OUTRO_TEXT: str
+    SETTINGS_HEADER: str
+    SETTINGS_SECTION: str
+    SETTINGS_TEXT: str
+    SETTINGS_OPTION: str
