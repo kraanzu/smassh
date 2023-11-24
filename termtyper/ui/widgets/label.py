f4c65f58c30112b2f6915fc0e44f545337b789c8
kraanzu <kraanzu@gmail.com>
2023-11-24 08:15:12 +0530

    feat: Keep navitem colored and bold when selected

--- termtyper/ui/widgets/label.py
+++ termtyper/ui/widgets/label.py
@@ -8,7 +8,6 @@ from termtyper.ui.events import SetScreen
 class Label(Widget):
     DEFAULT_CSS = """
     Label {
-        text-style: bold;
         content-align: center middle;
         width: auto;
         padding: 1;
@@ -45,3 +44,17 @@ class NavItem(Label):
     """
     Just a label widget with a callback
     """
+
+    DEFAULT_CSS = """
+    NavItem.active {
+        text-style: bold;
+        color: orange;
+    }
+    """
+
+    def on_click(self) -> None:
+        super().on_click()
+        if self.parent:
+            for item in self.parent.query(NavItem):
+                item.remove_class("active")
+            self.add_class("active")
