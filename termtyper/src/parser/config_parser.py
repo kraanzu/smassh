e33eee5c8f1f70208b973d749ca89a03db0280fe
kraanzu <kraanzu@gmail.com>
2023-12-02 17:18:20 +0530

    refactor: better config

--- termtyper/src/parser/config_parser.py
+++ termtyper/src/parser/config_parser.py
@@ -4,26 +4,25 @@ import appdirs
 
 DEFAULTS = {
     "difficulty": "normal",
-    "blind_mode": "off",
-    "min_speed": "0",
-    "min_accuracy": "0",
-    "min_burst": "0",
-    "force_correct": "off",
-    "confidence_mode": "off",
-    "capitalization_mode": "off",
-    "single_line_words": "off",
-    "cursor_buddy": "0",
-    "cursor_buddy_speed": "0",
-    "tab_reset": "off",
-    "restart_same": "off",
+    "blind_mode": False,
+    "min_speed": 0,
+    "min_accuracy": 0,
+    "min_burst": 0,
+    "force_correct": False,
+    "confidence_mode": False,
+    "capitalization_mode": False,
+    "single_line_words": False,
+    "cursor_buddy": 0,
+    "cursor_buddy_speed": 0,
+    "tab_reset": False,
+    "restart_same": False,
     "language": "english",
     "numbers": False,
     "punctuations": False,
     "caret_style": "block",
-    "bar_theme": "minimal",
     "writing mode": "words",
-    "paragraph_size": "teensy",
-    "timeout": "15",
+    "paragraph_size": 15,
+    "timeout": 15,
 }
 
 
@@ -33,14 +32,11 @@ class ConfigParser(Parser):
 
     def toggle_numbers(self):
         numbers = self.get("numbers")
-        self.set("numbers", "True" if str(numbers) == "False" else "False")
+        self.set("numbers", not numbers)
 
     def toggle_punctuations(self):
         punctuations = self.get("punctuations")
-        self.set(
-            "punctuations",
-            "True" if str(punctuations) == "False" else "False",
-        )
+        self.set("punctuations", not punctuations)
 
 
 config_parser = ConfigParser()
