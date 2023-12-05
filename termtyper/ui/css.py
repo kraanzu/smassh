df8746171dded6d78a980eb059a039ee33b653e4
kraanzu <kraanzu@gmail.com>
2023-12-05 21:03:13 +0530

    refactor: separte css folder for themes

--- termtyper/ui/css.py
+++ termtyper/ui/css.py
@@ -1,12 +1,28 @@
 import os
 from pathlib import Path
 
-parent_folder = Path(__file__).parent / "css"
+
+def load_folder(folder):
+    """
+    Load all css files in a folder and return them as a single string
+    """
+
+    css = ""
+
+    for file in os.listdir(folder):
+        if not file.endswith(".css"):
+            continue
+
+        with open(os.path.join(folder, file), "r") as css_file:
+            css += css_file.read()
+
+    return css
+
+
 CSS = ""
+css_folder = Path(__file__).parent / "css"
+theme_folder = css_folder / "themes"
 
-for file in os.listdir(parent_folder):
-    if not file.endswith(".css"):
-        continue
 
-    with open(os.path.join(parent_folder, file), "r") as css_file:
-        CSS += css_file.read()
+CSS += load_folder(css_folder)
+CSS += load_folder(theme_folder)
