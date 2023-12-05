e483b769d23228f6811c2bcb90464410b6b4829c
kraanzu <kraanzu@gmail.com>
2023-12-05 20:56:07 +0530

    feat: python script to combine all css files
    This script looks into all the css files and
    uses the python fd to read it as a string
    inside the `CSS` variable

--- termtyper/ui/css.py
+++ termtyper/ui/css.py
@@ -1,3 +1,12 @@
-CSS = """
+import os
+from pathlib import Path
 
-"""
+parent_folder = Path(__file__).parent / "css"
+CSS = ""
+
+for file in os.listdir(parent_folder):
+    if not file.endswith(".css"):
+        continue
+
+    with open(os.path.join(parent_folder, file), "r") as css_file:
+        CSS += css_file.read()
