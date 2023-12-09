4787446e31ac2ac901c167a7ba825d2f7f05e2b5
kraanzu <kraanzu@gmail.com>
2023-12-09 17:51:50 +0530

    feat: add app action to change theme

--- termtyper/ui/tui.py
+++ termtyper/ui/tui.py
@@ -2,6 +2,7 @@ import webbrowser
 from textual import on
 from textual.app import App, ComposeResult, events
 from textual.screen import Screen
+from textual.widget import Widget
 from textual.widgets import ContentSwitcher
 from termtyper.ui.css import CSS
 from termtyper.ui.events import SetScreen
@@ -46,5 +47,20 @@ class TermTyper(App):
     async def on_mount(self):
         self.push_screen(MainScreen())
 
+    async def on_ready(self):
+        self.action_theme(config_parser.theme)
+
     def action_sponsor(self):
         webbrowser.open("https://github.com/sponsors/kraanzu")
+
+    def action_theme(self, theme: str):
+        def remove_existing_theme(w: Widget):
+            for name in w.classes:
+                if name.startswith("theme-"):
+                    w.remove_class(name)
+                    return
+
+        with self.batch_update():
+            for widget in self.query("*"):
+                remove_existing_theme(widget)
+                widget.add_class(f"theme-{theme}")
