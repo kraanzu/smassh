cafb5a963114f4df907bf76f2d24d0e8b8d661f7
kraanzu <kraanzu@gmail.com>
2023-12-15 22:29:01 +0530

    refactor: cleanup and move to grid style

--- termtyper/ui/screens/typing.py
+++ termtyper/ui/screens/typing.py
@@ -1,10 +1,49 @@
+from rich.console import RenderableType
 from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Digits, Static
 from termtyper.ui.widgets.base_scroll import BaseWindow
 from termtyper.ui.widgets.config_strip import TypingConfigStrip
 from termtyper.ui.widgets.space import Space
 
 
-class TypingScreen(BaseWindow):
+class Pad(Widget):
+    DEFAULT_CSS = """
+    Pad.cspan3 {
+        column-span: 3;
+    }
+    """
+
+    def render(self) -> RenderableType:
+        return ""
+
+
+class TypingSpace(Widget):
+    DEFAULT_CSS = """
+    TypingSpace {
+        layout: grid;
+        grid-size: 3 6;
+        grid-columns: 1fr 4fr 1fr;
+        grid-rows: 1 1fr 1 3 1fr 1;
+        margin: 1;
+        height: 100%;
+        width: 100%;
+    }
+    """
+
     def compose(self) -> ComposeResult:
         yield TypingConfigStrip()
+        yield Pad(classes="cspan3")
+        yield Pad()
+        yield Static("23")
+        yield Pad()
+        yield Pad()
         yield Space()
+        yield Pad()
+        for i in range(13):
+            yield Pad()
+
+
+class TypingScreen(BaseWindow):
+    def compose(self) -> ComposeResult:
+        yield TypingSpace()
