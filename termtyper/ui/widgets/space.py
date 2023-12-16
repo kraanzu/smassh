a23684da05f9d2183f5bb051e3b186319d458df9
kraanzu <kraanzu@gmail.com>
2023-12-16 21:59:51 +0530

    feat: add scrolling

--- termtyper/ui/widgets/space.py
+++ termtyper/ui/widgets/space.py
@@ -1,25 +1,38 @@
-from bisect import bisect_left
+import operator
 import textwrap
+from bisect import bisect_right
+from itertools import accumulate
 from rich.console import RenderableType
-from rich.text import Text
+from rich.text import Span, Text
 from textual.widget import Widget
 from termtyper.src.generator import master_generator
 
 
 class Space(Widget):
+    DEFAULT_CSS = """
+    Space {
+        height: auto;
+    }
+    """
+
     def __init__(self):
         super().__init__()
         self.reset()
 
     @property
     def cursor_row(self):
-        return bisect_left(self.newlines, self.cursor)
+        return bisect_right(self.newlines, self.cursor)
 
     def on_show(self):
         paragraph = self.paragraph.plain
         width = self.size.width
         lines = textwrap.wrap(paragraph, width)
-        self.newlines = [len(line) for line in lines]
+        self.newlines = list(
+            accumulate(
+                [len(line) + 1 for line in lines],
+                operator.add,
+            )
+        )
 
     def reset(self) -> None:
         generated = master_generator.generate()
@@ -28,3 +41,20 @@ class Space(Widget):
 
     def render(self) -> RenderableType:
         return self.paragraph
+
+    def keypress(self, key: str) -> None:
+        current_row = self.cursor_row
+        correct = self.paragraph.plain[self.cursor] == key
+        self.paragraph.spans.append(
+            Span(
+                self.cursor,
+                self.cursor + 1,
+                "green" if correct else "red",
+            )
+        )
+
+        self.cursor += 1
+        if self.cursor_row != current_row:
+            self.parent.scroll_down()
+
+        self.refresh()
