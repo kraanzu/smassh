5c0714cbc1af631b362f414cd501cf593d3d3b09
kraanzu <kraanzu@gmail.com>
2023-12-20 07:27:08 +0530

    chore: add palette screens to module

--- termtyper/ui/screens/__init__.py
+++ termtyper/ui/screens/__init__.py
@@ -2,5 +2,13 @@ from .typing import TypingScreen
 from .help import HelpScreen
 from .about import AboutScreen
 from .settings import SettingsScreen
+from .palette import LanguagePaletteScreen, ThemePaletteScreen
 
-__all__ = ["TypingScreen", "HelpScreen", "AboutScreen", "SettingsScreen"]
+__all__ = [
+    "TypingScreen",
+    "HelpScreen",
+    "AboutScreen",
+    "SettingsScreen",
+    "LanguagePaletteScreen",
+    "ThemePaletteScreen",
+]
