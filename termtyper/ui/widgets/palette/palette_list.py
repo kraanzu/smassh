46fe6ee764c9851684f81988fba821a809f2cba8
kraanzu <kraanzu@gmail.com>
2023-12-25 22:44:35 +0530

    feat: add specific palette lists

--- termtyper/ui/widgets/palette/palette_list.py
+++ termtyper/ui/widgets/palette/palette_list.py
@@ -1,11 +1,22 @@
 from textual.app import ComposeResult
 from textual.widgets import Label, ListView, ListItem
+from termtyper.src.parser import config_parser
 
 
-class PaletteList(ListView):
+class PaletteList(ListView, can_focus=False):
     def get_options(self):
-        return ["english", "french"]
+        raise NotImplementedError
 
     def compose(self) -> ComposeResult:
         for option in self.get_options():
             yield ListItem(Label(option))
+
+
+class LanguagePaletteList(PaletteList):
+    def get_options(self):
+        return config_parser.configured_languages
+
+
+class ThemePaletteList(PaletteList):
+    def get_options(self):
+        return config_parser.configured_themes
