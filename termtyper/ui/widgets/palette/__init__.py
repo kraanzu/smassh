a58ebd69c47e56a545a4841a1737f5477ec57c4d
kraanzu <kraanzu@gmail.com>
2023-12-25 22:31:34 +0530

    chore: import specific palette menus

--- termtyper/ui/widgets/palette/__init__.py
+++ termtyper/ui/widgets/palette/__init__.py
@@ -1,10 +1,12 @@
-from .palette_list import PaletteList
+from .palette_list import PaletteList, LanguagePaletteList, ThemePaletteList
 from .palette_options import PaletteOptions
 from .palette_menu import PaletteMenu, LanguagePaletteMenu, ThemePaletteMenu
 from .palette_input import PaletteInput
 
 __all__ = [
     "PaletteList",
+    "LanguagePaletteList",
+    "ThemePaletteList",
     "PaletteOptions",
     "PaletteMenu",
     "LanguagePaletteMenu",
