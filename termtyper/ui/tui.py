6725cfd4640888c3fe40ab7b91c33197f431819d
kraanzu <kraanzu@gmail.com>
2024-01-04 09:14:18 +0530

    feat: better way of reading CSS
    This changes the CSS change method from copying the whole text
    and impl reading from source theme and base file and
    combining them both to `styles.tcss`

--- termtyper/ui/tui.py
+++ termtyper/ui/tui.py
@@ -1,4 +1,3 @@
-import shutil
 from pathlib import Path
 import webbrowser
 from textual import on
@@ -76,7 +75,16 @@ class TermTyper(App):
         css_folder = Path.absolute(Path(__file__).parent.parent) / "ui" / "css"
         themes_folder = css_folder / "themes"
 
-        base_css = css_folder / "styles.tcss"
+        base_path = css_folder / "base.css"
         theme_path = themes_folder / f"{theme}.tcss"
+        target_file = css_folder / "styles.tcss"
 
-        shutil.copy(theme_path, base_css)
+        with open(theme_path, "r") as theme_file:
+            theme_css = theme_file.read()
+
+        with open(base_path, "r") as base_file:
+            base_css = base_file.read()
+
+        with open(target_file, "w") as target:
+            target.write(theme_css)
+            target.write(base_css)
