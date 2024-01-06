f1a67584653f4a9304d3269f2e921b22cf3a3b53
kraanzu <kraanzu@gmail.com>
2024-01-06 19:22:15 +0530

    refactor: move static texts to CONSTANTS

--- termtyper/ui/screens/about.py
+++ termtyper/ui/screens/about.py
@@ -4,6 +4,22 @@ from textual.app import ComposeResult
 from textual.widget import Widget
 from termtyper.ui.widgets import BaseWindow
 
+DESCRIPTION = """
+Termtyper is a TUI typing application which was highly inspired
+by monkeytype -- An online web-based typing application which is
+by far the most customizable typing application
+
+Termtyper tries to bring features of monkeytype to terminal
+"""
+
+STAR = """
+[@click=app.star]Star this project on GitHub![/]
+"""
+
+OUTRO = """
+Made with ❤️  by kraanzu
+"""
+
 
 class Section(Widget):
     DEFAULT_CSS = """
@@ -23,31 +39,18 @@ class Section(Widget):
 
 class AboutDescription(Section):
     def __init__(self):
-        TEXT = """\
-    Termtyper is a TUI typing application which was highly inspired
-    by monkeytype -- An online web-based typing application which is
-    by far the most customizable typing application
-
-    Termtyper tries to bring features of monkeytype to terminal
-    """
-        super().__init__(TEXT)
+        super().__init__(DESCRIPTION)
         self.styles.padding = 2
 
 
 class Star(Section):
     def __init__(self):
-        TEXT = """\
-    [@click=app.star]Star this project on GitHub![/],
-    """
-        super().__init__(TEXT)
+        super().__init__(STAR)
 
 
 class AboutOutro(Section):
     def __init__(self):
-        TEXT = """\
-        Made with ❤️  by kraanzu
-        """
-        super().__init__(TEXT)
+        super().__init__(OUTRO)
 
 
 class AboutScreen(BaseWindow):
