e232d61a994e5fed4c9d51ff572b9f3f960594e1
kraanzu <kraanzu@gmail.com>
2024-01-07 20:16:33 +0530

    feat: basic skeleton application of typing mechanism

--- termtyper/ui/widgets/typing/space.py
+++ termtyper/ui/widgets/typing/space.py
@@ -18,13 +18,25 @@ class Space(Widget):
     def __init__(self):
         super().__init__()
         self.reset()
+        self.current_key = None
+
+    # ---------------- UTILS -----------------
 
     @property
     def cursor_row(self):
         return bisect_right(self.newlines, self.cursor)
 
     def reverse_span(self, pos: int) -> Span:
-        return Span(pos, pos + 1, "reverse")
+        return Span(pos, pos + 1, "reverse white")
+
+    def cursor_span(self, correct: bool) -> Span:
+        return Span(
+            self.cursor - 1,
+            self.cursor,
+            "green" if correct else "red",
+        )
+
+    # ----------------- RENDER ------------------
 
     def on_show(self):
         paragraph = self.paragraph.plain
@@ -46,23 +58,53 @@ class Space(Widget):
     def render(self) -> RenderableType:
         return self.paragraph
 
-    def update_colors(self, key: str) -> None:
-        correct = self.paragraph.plain[self.cursor] == key
-        self.paragraph.spans.append(
-            Span(
-                self.cursor,
-                self.cursor + 1,
-                "green" if correct else "red",
-            )
-        )
-        self.paragraph.spans.append(self.reverse_span(self.cursor + 1))
+    def update_cursor(self) -> None:
+        self.paragraph.spans.append(self.reverse_span(self.cursor))
+
+    def update_colors(self) -> None:
+        if not self.current_key:
+            return
+
+        self.paragraph.spans.pop()
+        key = self.current_key
+        spans = self.paragraph.spans
+
+        while spans and spans[-1].start >= self.cursor:
+            spans.pop()
+
+        if len(key) != 1:
+            return
+
+        # -1 because the cursor is ahead if a key is pressed
+        correct = self.paragraph.plain[self.cursor - 1] == key
+        self.paragraph.spans.append(self.cursor_span(correct))
+
+    # ---------------- KEYPRESS -----------------
+
+    def apply_key(self, key: str) -> None:
+        if key == "backspace":
+            self.cursor -= 1
+            return
+
+        self.cursor += 1
 
     def keypress(self, key: str) -> None:
-        self.paragraph.spans.pop()
+
+        if key == "space":
+            key = " "
+
         current_row = self.cursor_row
-        self.update_colors(key)
-        self.cursor += 1
-        if self.cursor_row != current_row and self.cursor_row > 1:
+        self.current_key = key
+
+        if (
+            self.cursor_row != current_row
+            and self.cursor_row > 1
+            and isinstance(self.parent, Widget)
+        ):
             self.parent.scroll_down()
+
+        self.apply_key(key)
+        self.update_colors()
+        self.update_cursor()
 
         self.refresh()
