9160ae1eb044e696f9b68ad6cdeec22436717f92
kraanzu <kraanzu@gmail.com>
2024-01-08 09:21:38 +0530

    chore: add basic `compose`

--- termtyper/ui/screens/result.py
+++ termtyper/ui/screens/result.py
@@ -1,3 +1,5 @@
+from textual.app import ComposeResult
+from textual.widgets import Label
 from termtyper.ui.widgets import BaseWindow
 
 
@@ -6,3 +8,6 @@ class ResultScreen(BaseWindow):
     This screen will show the result of the typing test.
     E.g. Typing Chart, Accuracy, WPM etc.
     """
+
+    def compose(self) -> ComposeResult:
+        yield Label("Result Screen")
