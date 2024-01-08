44f4aa3680f5e802c45ac2866cbc6a0484925d35
kraanzu <kraanzu@gmail.com>
2024-01-08 09:11:33 +0530

    feat: add `ResultScreen` skeleton

--- termtyper/ui/screens/__init__.py
+++ termtyper/ui/screens/__init__.py
@@ -2,10 +2,12 @@ from .typing import TypingScreen
 from .help import HelpScreen
 from .about import AboutScreen
 from .settings import SettingsScreen
+from .result import ResultScreen
 from .palette import LanguagePaletteScreen, ThemePaletteScreen
 
 __all__ = [
     "TypingScreen",
+    "ResultScreen",
     "HelpScreen",
     "AboutScreen",
     "SettingsScreen",
---
+++ termtyper/ui/screens/result.py
@@ -0,0 +1,8 @@
+from termtyper.ui.widgets import BaseWindow
+
+
+class ResultScreen(BaseWindow):
+    """
+    This screen will show the result of the typing test.
+    E.g. Typing Chart, Accuracy, WPM etc.
+    """
