9b21004a7a8f015fed41c8ee19090481e00c6e45
kraanzu <kraanzu@gmail.com>
2024-01-08 15:54:51 +0530

    feat: add `ShowResult` event to show typing results

--- termtyper/ui/events.py
+++ termtyper/ui/events.py
@@ -1,7 +1,14 @@
 from textual.message import Message
+from termtyper.src import StatsTracker
 
 
 class SetScreen(Message):
     def __init__(self, screen_name: str) -> None:
         super().__init__()
         self.screen_name = screen_name
+
+
+class ShowResults(Message):
+    def __init__(self, stats: StatsTracker) -> None:
+        super().__init__()
+        self.stats = stats
