03529c1610bcfecc728bc06d4e8a85f16f5d5275
kraanzu <kraanzu@gmail.com>
2024-01-10 22:08:27 +0530

    feat: impl `numbers` setting

--- termtyper/src/generator.py
+++ termtyper/src/generator.py
@@ -8,6 +8,29 @@ PUNCS = ",.;?!"
 GeneratorFunc = Callable[["Generator"], str]
 
 
+def numbers(func: GeneratorFunc) -> GeneratorFunc:
+    def wrapper(*args, **kwargs):
+        paragraph = func(*args, **kwargs)
+
+        if config_parser.get("numbers"):
+
+            words = paragraph.split()
+            total_words = len(words)
+            words_to_insert = total_words // 5
+            positions = sample(range(total_words), words_to_insert)
+            positions.sort()
+
+            for position in positions:
+                numbers = [str(randint(10**i, 10 ** (i + 1))) for i in range(4)]
+                words[position] = choice(numbers)
+
+            return " ".join(words)
+
+        return paragraph
+
+    return wrapper
+
+
 def punctuations(func: GeneratorFunc) -> GeneratorFunc:
     def wrapper(*args, **kwargs):
         paragraph = func(*args, **kwargs)
@@ -45,6 +68,7 @@ class Generator:
     def words(self) -> str:
         return " ".join(sample(english_words.split(), 64))
 
+    @numbers
     @punctuations
     def generate(self) -> str:
         return self.words
