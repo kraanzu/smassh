df7ffd5a966f74bb01e6e406c01c1008aa5f1767
kraanzu <kraanzu@gmail.com>
2024-01-10 22:01:37 +0530

    feat: impl setting for punctuations

--- termtyper/src/generator.py
+++ termtyper/src/generator.py
@@ -1,8 +1,40 @@
+from collections.abc import Callable
 from termtyper.assets.words import *  # noqa
-from random import sample
+from termtyper.src.parser import config_parser
+from random import randint, choice, sample
 
 
-PUNCS = "$%&'(),-.:;?"
+PUNCS = ",.;?!"
+GeneratorFunc = Callable[["Generator"], str]
+
+
+def punctuations(func: GeneratorFunc) -> GeneratorFunc:
+    def wrapper(*args, **kwargs):
+        paragraph = func(*args, **kwargs)
+
+        if config_parser.get("punctuations"):
+            words = paragraph.split()
+            new_paragraph = []
+
+            for word in words:
+                i = randint(0, 20)
+
+                if i == 0:
+                    word = word + choice(PUNCS)
+                elif i == 1:
+                    word = f"'{word}'"
+                elif i == 2:
+                    word = f'"{word}"'
+                elif i == 3:
+                    word = f"({word})"
+
+                new_paragraph.append(word)
+
+            paragraph = " ".join(new_paragraph)
+
+        return paragraph
+
+    return wrapper
 
 
 class Generator:
@@ -13,60 +45,9 @@ class Generator:
     def words(self) -> str:
         return " ".join(sample(english_words.split(), 64))
 
+    @punctuations
     def generate(self) -> str:
         return self.words
 
 
 master_generator = Generator()
-
-# def generate_paragraph(
-#     times: int = 1,
-#     numbers: bool = False,
-#     punctuations: bool = False,
-#     capitalizitions: str = "off",
-#     language: str = "english",
-# ) -> str:
-#     """
-#     produces a paragraph
-#     """
-#     times *= 30
-#     extra = []
-#     if numbers:
-#         extra.extend([str(choice(range(1, 10000))) for _ in " " * 200])
-#     if punctuations:
-#         extra.extend([choice(PUNCS) for _ in " " * 200])
-#
-#     match language:
-#         case "french":
-#             words = french_words
-#         case _:
-#             words = english_words
-#
-#     words = words.split()
-#     arr = [choice(words + extra) for _ in " " * times]
-#     para = ""
-#     for i in arr:
-#         if i in PUNCS:
-#             para += i
-#         else:
-#             match capitalizitions:
-#                 case "on":
-#                     if random() > 0.7:
-#                         i = i.capitalize()
-#                 case "max":
-#                     r = random()
-#                     if r > 0.7:
-#                         i = i.capitalize()
-#                     elif r > 0.6:
-#                         i = i.upper()
-#                     elif r > 0.4:
-#                         upper_indexs = [
-#                             randint(0, len(i) - 1) for _ in range(randint(1, len(i)))
-#                         ]
-#                         for index in upper_indexs:
-#                             i = i[:index] + i[index].upper() + i[index + 1 :]
-#                 case _:
-#                     pass
-#             para += " " + i
-#
-#     return para.strip()
