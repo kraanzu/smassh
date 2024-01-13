f31fb36ea5c4887e1f61d9b300ac4f3f7ec6ac36
kraanzu <kraanzu@gmail.com>
2024-01-13 09:01:24 +0530

    feat: impl stop when typing is finished

--- termtyper/ui/widgets/typing/ticker.py
+++ termtyper/ui/widgets/typing/ticker.py
@@ -1,5 +1,6 @@
 from textual.widget import Widget
 from termtyper.src import config_parser
+from termtyper.ui.events import ShowResults
 
 
 class Ticker(Widget):
@@ -13,15 +14,22 @@ class Ticker(Widget):
 
         mode = config_parser.get("mode")
         stats = self.screen.query_one(Space).tracker.stats
+
         if mode == "words":
             count = config_parser.get("words_count")
             words_typed = stats.word_count
+            finished = words_typed == count
             self.text = f"{words_typed}/{count}"
         else:
+            finished = False
             if stats.start_time:
                 count = config_parser.get("time_count")
                 time_remaining = count - stats.elapsed_time
+                finished = time_remaining <= 0
                 self.text = str(round(time_remaining))
+
+        if finished:
+            self.screen.post_message(ShowResults(stats))
 
         self.refresh()
 
