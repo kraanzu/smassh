e5fa5780c1b593e656501071204a406d4d0abaab
kraanzu <kraanzu@gmail.com>
2024-01-18 22:26:49 +0530

    feat(beta): basic result screen
    add WPM and ACC

--- termtyper/ui/screens/result.py
+++ termtyper/ui/screens/result.py
@@ -1,8 +1,33 @@
 from textual.app import ComposeResult
-from textual.widgets import Label
+from textual.widgets import Digits, Label, Static
 from termtyper.src.stats_tracker import StatsTracker
 from termtyper.ui.widgets import BaseWindow
 from termtyper.ui.widgets.typing.space import Space
+
+
+class ValueLabel(Label):
+    pass
+
+
+class Value(Digits, Static):
+    DEFAULT_CSS = """
+    Value {
+        margin: 0 2;
+        content-align: right middle;
+        height: auto;
+        width: auto;
+    }
+    """
+
+
+class AutoVertical(Static):
+    DEFAULT_CSS = """
+    AutoVertical {
+        margin: 1;
+        height: auto;
+        width: auto;
+    }
+    """
 
 
 class ResultScreen(BaseWindow):
@@ -18,11 +43,24 @@ class ResultScreen(BaseWindow):
     }
     """
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.wpm = Value()
+        self.accuracy = Value()
+
+    def compose(self) -> ComposeResult:
+        with AutoVertical():
+            yield ValueLabel("WPM")
+            yield self.wpm
+
+        with AutoVertical():
+            yield ValueLabel("ACC")
+            yield self.accuracy
+
     def on_show(self):
         self.app.query_one(Space).reset()
 
-    def compose(self) -> ComposeResult:
-        yield Label("Result Screen")
-
     def set_results(self, stats: StatsTracker):
         self.stats = stats
+        self.wpm.update(str(stats.wpm))
+        self.accuracy.update(str(stats.accuracy))
