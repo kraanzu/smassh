919ba10624a07cec10c8264ca93020998db377e7
duwudle <duwudle@gmail.com>
2024-01-19 11:35:27 +0530

    feat: Use click for parsing arguments rather than `argparse`

--- termtyper/__init__.py
+++ termtyper/__init__.py
@@ -1,22 +1,13 @@
-from importlib.metadata import version
-import argparse
+from importlib.metadata import version as getVersion
+import click
 
 from .ui import TermTyper
 
-
-def main():
-    args = argparse.ArgumentParser()
-    args.add_argument(
-        "-v",
-        "--version",
-        help="Show version",
-        action="store_true",
-    )
-
-    args = args.parse_args()
-
-    if args.version:
-        ver = version("termtyper")
+@click.command
+@click.option("--version", "-v", is_flag=True, help="Show version", default=False)
+def main(version):
+    if(version):
+        ver = getVersion("termtyper")
         print(f"termtyper - {ver}")
         return
 
