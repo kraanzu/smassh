f6ef94067a60e77e3585deab3458c16b1d86784e
kraanzu <kraanzu@gmail.com>
2024-01-20 08:41:13 +0530

    feat: add click callbacks to result strip

--- termtyper/ui/widgets/result/result_strip.py
+++ termtyper/ui/widgets/result/result_strip.py
@@ -1,6 +1,8 @@
 from rich.console import RenderableType
 from textual.app import ComposeResult
 from textual.widgets import Label, Static
+from termtyper.ui.events import SetScreen
+from termtyper.ui.widgets import Space
 
 
 class ResultStripItem(Label):
@@ -24,10 +26,18 @@ class NextTest(ResultStripItem):
     icon = ""
     help = "Next test"
 
+    def on_click(self, _) -> None:
+        self.screen.query_one(Space).reset()
+        self.post_message(SetScreen("typing"))
+
 
 class RepeatTest(ResultStripItem):
     icon = ""
     help = "Repeat test"
+
+    def on_click(self, _) -> None:
+        self.screen.query_one(Space).restart()
+        self.post_message(SetScreen("typing"))
 
 
 class ResultStrip(Static):
