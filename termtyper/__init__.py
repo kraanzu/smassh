e70396991ec02f597e21d76c8bfc05925b9e2112
duwudle <duwudle@gmail.com>
2024-01-20 21:12:35 +0530

    feat: Add add `-v` and `-h` flags for version and help respectively

--- termtyper/__init__.py
+++ termtyper/__init__.py
@@ -1,9 +1,12 @@
-from importlib.metadata import version
+from importlib.metadata import version as pkgVersion
 import click
 
 from .ui import TermTyper
 
-@click.command
-@click.version_option(version("termtyper"), prog_name="termtyper")
-def main():
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option("--version", "-v", is_flag=True, help="Show version and exit.")
+def main(version: bool):
+    if(version):
+        return print(f"termtyper - v{pkgVersion('termtyper')}")
+
     TermTyper().run()
