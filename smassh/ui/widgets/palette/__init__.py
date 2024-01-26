14b12014b6786fc05d632129407a4917aa4a7f16
kraanzu <kraanzu@gmail.com>
2024-01-26 15:24:30 +0530

    cleanup: remove unused code

--- smassh/ui/widgets/palette/__init__.py
+++ smassh/ui/widgets/palette/__init__.py
@@ -1,6 +1,5 @@
 from .palette_list import PaletteList, LanguagePaletteList, ThemePaletteList
 from .palette_options import PaletteOptions, LanguagePalette, ThemePalette
-from .palette_menu import PaletteMenu, LanguagePaletteMenu, ThemePaletteMenu
 from .palette_input import PaletteInput
 
 __all__ = [
@@ -10,8 +9,5 @@ __all__ = [
     "PaletteOptions",
     "LanguagePalette",
     "ThemePalette",
-    "PaletteMenu",
-    "LanguagePaletteMenu",
-    "ThemePaletteMenu",
     "PaletteInput",
 ]
