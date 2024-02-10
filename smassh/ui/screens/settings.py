28697aa3ef1a038c9632da9846fe9e437385e186
kraanzu <kraanzu@gmail.com>
2024-02-10 12:23:27 +0530

    feat: basic css for setting separtors

--- smassh/ui/screens/settings.py
+++ smassh/ui/screens/settings.py
@@ -11,20 +11,24 @@ from textual.widget import Widget
 class SettingSeparator(Widget):
     DEFAULT_CSS = """
     SettingSeparator {
-        padding: 1;
         height: auto;
         width: 100%;
         text-style: bold italic;
-        margin: 0 1;
+        margin: 1 0;
     }
     """
+
+    COMPONENT_CLASSES = {"--primary-bg", "--danger-bg"}
 
     def __init__(self, section: str, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.section = section
 
     def render(self) -> RenderableType:
-        text = Text(self.section.upper())
+        classname = "danger" if "danger" in self.section else "primary"
+        style = self.get_component_rich_style(f"--{classname}-bg")
+
+        text = Text(self.section.upper(), style=style)
         text.pad(1)
         return text
 
