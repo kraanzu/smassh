56cd74aed3a1f42c84b3cd0fe27ab2b4585bc11e
kraanzu <kraanzu@gmail.com>
2024-02-10 17:47:19 +0530

    refactor: move setting sections to `widgets`

---
+++ smassh/ui/widgets/settings/separator.py
@@ -0,0 +1,28 @@
+from rich.console import RenderableType
+from rich.text import Text
+from textual.widget import Widget
+
+
+class SettingSeparator(Widget):
+    DEFAULT_CSS = """
+    SettingSeparator {
+        height: auto;
+        width: 100%;
+        text-style: bold italic;
+        margin: 1 0;
+    }
+    """
+
+    COMPONENT_CLASSES = {"--primary-bg", "--danger-bg"}
+
+    def __init__(self, section: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.section = section
+
+    def render(self) -> RenderableType:
+        classname = "danger" if "danger" in self.section else "primary"
+        style = self.get_component_rich_style(f"--{classname}-bg")
+
+        text = Text(self.section.upper(), style=style)
+        text.pad(1)
+        return text
