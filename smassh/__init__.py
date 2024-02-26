2cad495f4a2a33f311d1132beeb2dec32be8ecef
kraanzu <kraanzu@gmail.com>
2024-02-26 14:46:30 +0530

    fix: workflow for app build

---
+++ smassh/__init__.py
@@ -0,0 +1,34 @@
+import click
+from smassh.src.plugins.add_language import AddLanguage
+from smassh.ui.tui import Smassh
+
+PKG_VERSION = "3.1.0"
+
+
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+@click.option(
+    "--version",
+    "-v",
+    is_flag=True,
+    help="Show version and exit.",
+)
+@click.pass_context
+def main(ctx, version: bool) -> None:
+    if version:
+        return print(f"smassh - v{PKG_VERSION}")
+
+    if ctx.invoked_subcommand is None:
+        Smassh().run()
+
+
+@main.command(help="Add a language to smassh")
+@click.argument("name")
+def add(name: str) -> None:
+    AddLanguage().add(name)
+
+
+if __name__ == "__main__":
+    main()
